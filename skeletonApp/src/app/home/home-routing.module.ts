c264356bc754ba80ea489ebe7bb35a7d25468ead
munoz.mora.nicolas <munoz.mora.nicolas@gmail.com>
2023-08-29 17:25:36 -0400

    fix

--- skeletonApp/src/app/home/home-routing.module.ts
+++ skeletonApp/src/app/home/home-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
-import { PerfilComponent } from './perfil/perfil.component';
-import { AsistenciaComponent } from './asistencia/asistencia.component';
 
 
 const routes: Routes = [
@@ -10,14 +8,6 @@ const routes: Routes = [
     path: '',
     component: HomePage,
     children:[
-      {
-        path: 'asistencia',
-        component: AsistenciaComponent
-      },
-      {
-        path: 'perfil',
-        component:PerfilComponent
-      },
       {
         path: '',
         redirectTo: 'perfil',
